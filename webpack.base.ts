12676e0fb5ba8a6284960fbe551b3e76c8fc0e58
Evgenii Kalkutin <evgheni.calcutin@gmail.com>
2025-03-30 18:57:36 +0300

    Add webpack configs

---
+++ webpack.base.ts
@@ -0,0 +1,61 @@
+import path from 'path';
+
+import { Configuration } from 'webpack';
+
+const isProduction = process.env.NODE_ENV === 'production';
+
+const configuration: Configuration = {
+  mode: isProduction ? 'production' : 'development',
+  devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+    filename: '[name].bundle.js',
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        loader: 'esbuild-loader',
+        options: {
+          loader: 'tsx',
+        },
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg|webp)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/images/[name][ext]',
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/fonts/[name][ext]',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      components: path.resolve(__dirname, 'src', 'components'),
+    },
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+};
+
+export default configuration;
